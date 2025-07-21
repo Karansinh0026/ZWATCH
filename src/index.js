2baef4bd9b3795931633df7d99e905bb01646f40
Sarvaiya Karansinh <karansinhsarvaiya260905@gmail.com>
2025-07-21 23:12:18 +0530

    utils files added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import dotenv from 'dotenv'
 import { DB_NAME} from './constants.js';
 import connectDB from './db/index.js';
+import app from './app.js';
+
+
+
 dotenv.config({path:"./.env"})
- connectDB()
 
-
+connectDB()
+.then(()=>{
+    app.on('error',(error)=>{
+        console.log('error occured',error)
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port:${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Mongo DB connection Failed !!!",error)
+})
 
 
 // import express from 'express'
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,26 @@
+// this is used to solve the await problem
+
+
+// using try catch method
+// const asynchandler = (func) => async(req,res,next) =>{
+//     try {
+//         await func(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             sucess:false,
+//             message:err.message
+//         })
+//     }
+// }
+
+
+// Using promise 
+const asynchandler = (requesthandler)=>{
+    (req,res,next) =>{
+        Promise.resolve(requesthandler(req,res,next)).catch((err)=>{next(err)})
+    }
+}
+
+
+
+export {asynchandler}
